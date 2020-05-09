7ceb3f9eca0da0b61e79fa2dc6ada60bb56dc3c6
Neil Berg <neil.berg14@gmail.com>
2020-05-09 15:28:17 -0700

    shift to vimeo storage

--- src/data/members.ts
+++ src/data/members.ts
@@ -1,12 +1,30 @@
 /* eslint-disable quotes */
 import { Member } from '../types';
 
+export const hannahEntry: Member = {
+  id: 12,
+  name: 'Hannah Eagle',
+  labName: 'Fish Out Of Water',
+  videoSrc: 'https://player.vimeo.com/video/416736983',
+};
+
+export const jakeEntry: Member = {
+  id: 13,
+  name: 'Jake Eagle',
+  labName: "I'm Not Here",
+  videoSrc: 'https://player.vimeo.com/video/416752953',
+  quote:
+    'Gathered together, we find our light. And each spark shifts \
+    and multiplies, scattering its radiance on our ordinary lives. Like \
+    everything precious, more valuable when shared. -Jennifer Worth',
+};
+
 export const memberData: Member[] = [
   {
     id: 1,
     name: 'Carol Richmond',
     labName: 'Witness',
-    videoFile: 'carol-richmond-video.mp4',
+    videoSrc: 'https://player.vimeo.com/video/416746221',
     quote:
       'It is only through a change of consciousness and purposeful living that the world will survive...and maybe even thrive.',
   },
@@ -14,65 +32,65 @@ export const memberData: Member[] = [
     id: 2,
     name: 'Katharine Rivers',
     labName: 'Morning',
-    videoFile: 'katharine-rivers-video.mp4',
+    videoSrc: 'https://player.vimeo.com/video/416746559',
   },
   {
     id: 3,
     name: 'Amber Sky',
     labName: 'Pliable',
-    videoFile: 'amber-sky-video.mp4',
+    videoSrc: 'https://player.vimeo.com/video/416729427',
   },
   {
     id: 4,
     name: 'Shawn Newell',
     labName: 'Gypsy Loon',
-    videoFile: 'shawn-newell-video.mp4',
+    videoSrc: 'https://player.vimeo.com/video/416746910',
     imageFile: 'shawn-newell-image-2.jpg',
   },
   {
     id: 5,
     name: 'Deena Flemming',
     labName: 'Meta Lark',
-    videoFile: 'deena-fleming-video.mp4',
-    imageFile: 'deena-fleming-image.jpg',
+    videoSrc: 'https://player.vimeo.com/video/416747648',
+    imageFile: 'deena-fleming-image-2.jpg',
   },
   {
     id: 6,
     name: 'Jessica Gennari',
     labName: 'Jessica',
-    videoFile: 'jessica-gennari-video.mp4',
+    videoSrc: 'https://player.vimeo.com/video/416748131',
   },
   {
     id: 7,
     name: 'Michelle Martinez',
     labName: 'All In',
-    videoFile: 'michelle-martinez-video.mp4',
+    videoSrc: 'https://player.vimeo.com/video/416749697',
     imageFile: 'michelle-martinez-image-2.jpg',
   },
   {
     id: 8,
     name: 'Cheryl Handel',
     labName: 'Voice',
-    videoFile: 'cheryl-handel-video.mp4',
+    videoSrc: 'https://player.vimeo.com/video/416750336',
   },
   {
     id: 9,
     name: 'Dave Miescke',
     labName: 'Loving Life',
-    videoFile: 'dave-miescke-video.mp4',
+    videoSrc: 'https://player.vimeo.com/video/416751126',
     imageFile: 'dave-miescke-image.jpg',
   },
   {
     id: 10,
     name: 'Yasemin Turkell',
     labName: 'Namaste',
-    videoFile: 'yasemin-turkell-video.mp4',
+    videoSrc: 'https://player.vimeo.com/video/416751848',
     imageFile: 'yasemin-turkell-image-2.jpg',
   },
   {
     id: 11,
     name: 'Barbara Holtzman',
     labName: 'Inspired',
-    videoFile: 'barbara-holtzman-video.mp4',
+    videoSrc: 'https://player.vimeo.com/video/416752495',
   },
 ];
